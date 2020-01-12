2cb9c2704a084b3884a95540524776178bdeb9ea
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-12 22:07:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Artist
   @@all = []
   attr_accessor :name, :songs
@@ -35,3 +36,33 @@ class Artist
     songs.each {|song| puts song.name}
   end
 end
+=======
+class Artist 
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @songs = []
+    @save = save 
+  end 
+  def self.all
+    @@all
+  end 
+   def songs 
+     Song.all.select{|song| song.artist == self}
+   end   
+  def add_song(song)
+    song.artist = self 
+    song 
+  end 
+  def self.find_or_create_by_name(name)
+     self.all.detect {|artist| artist.name == name} || Artist.new(name)
+end 
+ def save
+    @@all << self
+  end
+def print_songs
+   songs.each {|x| puts x.name}
+end   
+end 
+>>>>>>> ae1dad0be8a1e837d93661f1ccedf4d95593fc0f
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist
+<<<<<<< HEAD
   @@all = []
 
   def initialize(name)
@@ -27,3 +28,23 @@ class Song
     artist.add_song(self)
   end
 end
+=======
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
+
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+end
+>>>>>>> ae1dad0be8a1e837d93661f1ccedf4d95593fc0f
